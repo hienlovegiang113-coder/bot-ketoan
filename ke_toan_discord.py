d6525460d22a05118e640bfa82cca0e4aebb3f72
hienlovegiang113-coder <hienlovegiang113@gmail.com>
2026-02-11 13:42:59 +0900

    Update ke_toan_discord.py

--- ke_toan_discord.py
+++ ke_toan_discord.py
@@ -7,7 +7,6 @@ from datetime import datetime
 # đường dẫn tesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-import os
 TOKEN = os.getenv("TOKEN")
 
 CHANNEL_THUE_XE = 1439183974646681641
@@ -133,5 +132,6 @@ async def on_ready():
     print("🔥 BOT ONLINE!!!")
     daily_report.start()
 bot.run(TOKEN)
+
 
 
